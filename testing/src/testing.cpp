dc5f24deb0a63686728641ce056093cc5c4c4c4a
Kyotun <emirpisirici@hotmail.com>
2023-10-14 10:01:38 +0200

    Hey. A little bit work on references and pointers. That was a really good one. I did it without even look anywhere just by thinking and trying.

--- testing/src/testing.cpp
+++ testing/src/testing.cpp
@@ -7,13 +7,26 @@
 //============================================================================
 
 #include <iostream>
-
 using namespace std;
 
 int main() {
+	int a = 5;
+	int& b = a;
+	int* c = &a;
+	cout << "Das ist b, auch der Wert von a: " << b << endl;
+	cout << "Das ist c, auch die Adresse von a: " << c << endl;
 
-	int array_one[2] = {1,2};
-	cout << array_one[1] << endl;
-	cout << "!!!Hello Worldasdasd!!!" << endl; // prints !!!Hello World!!!
+	b = 10;
+	cout << "Das ist der neue Wert von a: " << a << endl;
+	cout << "Wie sieht dann b aus?: " << b << endl;
+
+	int &d = *c;
+	cout << "a: " << a << ", b: " << b << ", c: " << c << ", d: " << d << endl;
+
+	d = 15;
+	cout << "a: " << a << ", b: " << b << ", c: " << c << ", d: " << d << endl;
+	*c = 5;
+	cout << "a: " << a << ", b: " << b << ", c: " << c << ", d: " << d << endl;
+
 	return 0;
 }
