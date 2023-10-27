d4d24ccbfe879bc91d0730f60f885072c83042b6
Kyotun <emirpisirici@hotmail.com>
2023-10-27 14:58:27 +0200

    Overload of operator '<<'

--- testing/src/testing.cpp
+++ testing/src/testing.cpp
@@ -7,82 +7,161 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
+#include<iomanip>
+
 using namespace std;
 
-
-class Base
+class Train
 {
+	protected:
+		float p_fDelay = 0.0;
 	private:
-		double p_x = 0.0;
-		double p_y = 0.0;
+		static inline int p_iIDCounter = 0;
+		const int p_iID = p_iIDCounter;
+
 	public:
-		virtual void method() const;
-		virtual void method_zwei() const;
-		void method_drei();
-		void method_vier() const;
-		virtual ~Base() = default;
+		Train() = default;
+		Train(int speed, bool electric);
+		virtual ~Train() = default;
+
+		virtual void vPrintProperties(std::ostream& ausgabe) const;
 
 };
 
-void Base::method() const
+void Train::vPrintProperties(std::ostream& ausgabe) const
 {
-	cout << "Base" << endl;
+	cout << "ID: " << p_iID << endl;
+	cout << "Momentane Verspeatung: " << setprecision(3) << p_fDelay << " Minuten" << endl;
 }
 
-void Base::method_zwei() const
-{
-	cout << "Base_zwei" << endl;
-}
 
-void Base::method_drei()
-{
-	cout << "Base_drei" << endl;
-}
-
-void Base::method_vier() const
-{
-	cout << "Base_vier" << endl;
-}
-class Derived : public Base
+class PassengerTrain : public Train
 {
 	private:
-		double p_x = 0.0;
-		double p_y = 0.0;
+		int p_iPassengerCounter = 0;
 	public:
-		void method() const;
-		void method_zwei();
-		void method_drei();
-		void method_vier() const;
+		PassengerTrain() = default;
+		PassengerTrain(int passengers);
+		virtual ~PassengerTrain() = default;
+
+		float fStation(int passengersIn, int passengersOut);
+		float getVerspeatung();
+		void vPrintProperties(std::ostream& ausgabe) const;
 };
 
-void Derived::method() const
+PassengerTrain::PassengerTrain(int passengers)
 {
-	cout << "Derived" << endl;
+	cout << "Aufruf des Nicht-Standardkonstruktors(PassengerTrain)" << endl;
+	this->p_iPassengerCounter = passengers;
 }
 
-void Derived::method_zwei(){
-	cout << "Derived_zwei" << endl;
-}
-
-void Derived::method_drei()
+float PassengerTrain::fStation(int passengersIn, int passengersOut)
 {
-	cout << "Derived_drei" << endl;
+	if(p_iPassengerCounter < passengersOut){
+		cout << "Es koennen nicht mehr Personen aussteigen als sich im Zug befinden" << endl;
+		return 0;
+	}
+
+	p_iPassengerCounter = p_iPassengerCounter + passengersIn - passengersOut;
+
+	float secondsChange = 120 - passengersIn*10 - passengersOut*5;
+	p_fDelay = (p_fDelay - secondsChange < 0)? 0: (p_fDelay - secondsChange)/60;
+
+	return secondsChange/60;
 }
 
-void Derived::method_vier() const
+void PassengerTrain::vPrintProperties(std::ostream& ausgabe) const
 {
-	cout << "Derived_vier" << endl;
-
+	Train::vPrintProperties(ausgabe);
+	cout << "Anzahl der Passegiere: " << p_iPassengerCounter << endl;
 }
+
+float PassengerTrain::getVerspeatung()
+{
+	return p_fDelay;
+}
+
+class FreightTrain : public Train
+{
+	private:
+		float p_fChargeQuantity = 0.0;
+		string p_sChargeType = "default";
+
+	public:
+		FreightTrain() = default;
+		FreightTrain(float chargeQuantity, string chargeType);
+		virtual ~FreightTrain() = default;
+
+		float fStation(int aufladendeGueter, int abzuladendeGueter);
+		void vPrintProperties(std::ostream& ausgabe) const;
+		void vLoadCharge(float amount);
+
+};
+
+FreightTrain::FreightTrain(float chargeQuantity, string chargeType)
+{
+	this->p_fChargeQuantity = chargeQuantity;
+	this->p_sChargeType = chargeType;
+}
+
+void FreightTrain::vPrintProperties(std::ostream& ausgabe) const
+{
+	Train::vPrintProperties(ausgabe);
+	cout << "Ladung: " << p_fChargeQuantity << " " << p_sChargeType << endl;
+}
+
+void FreightTrain::vLoadCharge(float amount)
+{
+	if(amount + p_fChargeQuantity < 0){
+		cout << "Es kann nicht mehr abgeladen werden" << endl;
+		return;
+	}
+
+	cout << "Charge Quantity war: " << p_fChargeQuantity << endl;
+
+	p_fChargeQuantity += amount;
+
+	if(amount < 0){
+		cout << "Eine Menge von " << -amount << "  abgeladen." << endl;
+		cout << "Momentane Ladung: " << p_fChargeQuantity << endl;
+	}else if(amount == 0){
+		cout << "Keine Auf-/Abladung." << endl;
+	}else {
+		cout << "Eine Menge von " << amount << " aufgeladen." << endl;
+		cout << "Momentane Ladung: " << p_fChargeQuantity << endl;
+	}
+}
+
+#define OPERATOR operator<<
+std::ostream & OPERATOR(std::ostream& out, Train& train)
+{
+	train.vPrintProperties(out);
+	return out;
+}
+#undef OPERATOR
 
 int main()
 {
-	Derived d;
-	Base& base = d;
-	base.method();
-	base.method_zwei();
-	base.method_drei();
-	base.method_vier();
+	PassengerTrain pt = PassengerTrain(30);
+	FreightTrain ft = FreightTrain(15, "Kisten Schokolade");
+
+	cout << endl << "Eigenschaften PT: " << endl;
+	cout << pt;
+	cout << endl << "10 Passagiere steigen ein und 5 Passagiere steigen ab." << endl;
+	pt.fStation(20,5);
+	cout << "Totale Verpseatung dafür: " << setprecision(2) << pt.getVerspeatung() << " Minuten." << endl;
+
+	cout << endl << "Eigenschaften FT: " << endl;
+	cout << ft;
+	cout << endl << "Batterie Zustand des FT nach 10 Kisten Schokolade"
+			"ablageden wurden: " << endl;
+	ft.vLoadCharge(-10);
+	cout << endl << "Batterie Zustand des FT nach 32 Kisten Schokolade "
+			"auflageden wurden: " << endl;
+	ft.vLoadCharge(32);
+
+
 
 	return 0;
 
