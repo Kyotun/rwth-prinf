66053b16f1545a94a879aa627ef459c2edf3edc0
Kyotun <emirpisirici@hotmail.com>
2023-10-28 14:06:26 +0200

    weak-ptr with shared-ptr and understanding the scope in main function

--- testing/src/testing.cpp
+++ testing/src/testing.cpp
@@ -14,53 +14,30 @@
 
 using namespace std;
 
-class Person
-{
-    public :
-        Person() = delete ;
-        Person(std::string name , std::shared_ptr<Person> father = nullptr , std::shared_ptr<Person> mother = nullptr) ;
-        virtual ~Person() ;
-        void setSibling(std::weak_ptr<Person> sibling);
-    private :
-        std::shared_ptr<Person> p_father;
-        std::shared_ptr<Person> p_mother;
-        std::string p_sName;
-        std::vector<std::weak_ptr<Person>> p_siblings;
-};
-
-Person::Person(string name, shared_ptr<Person> father, shared_ptr<Person> mother){
-	p_sName = name;
-	p_father = father;
-	p_mother = mother;
-}
-
-void Person::setSibling(weak_ptr<Person> brosis){
-	p_siblings.push_back(brosis);
-}
-
-Person::~Person(){
-	p_siblings.clear();
-	std::cout << "Geloescht: " << p_sName << std::endl;
-}
-
-void test1 ()
-{
-    auto m1 = std::make_shared<Person>("Josef");
-    auto f1 = std::make_shared<Person>("Maria");
-    auto m2 = std::make_shared<Person>("Peter", m1 , f1 );
-    auto f2 = std::make_shared<Person>("Birgit", m1 , f1 );
-    m2->setSibling(f2);
-    f2->setSibling(m2);
+void doesWeakExistProperly(std::weak_ptr<int> weak){
+    if(weak.lock()){
+        std::cout << "Der Pointer wurde erfolgreich gelocked und hat den Inhalt: " << *weak.lock() << std::endl;
+    } else {
+        std::cout << "Der Pointer konnte nicht gelocked werden." << std::endl;
+    }
 }
 
 
 int main()
 {
-
-	std::cout << "Anfang" << std::endl;
-	test1();
-	std::cout << "Ende" << std::endl;
-
+	std::weak_ptr<int> weak;
+	    doesWeakExistProperly(weak);
+	    {
+	        auto shared = std::make_shared<int>(42);
+	        weak = shared;
+	        std::cout << "Der Pointer `weak` ist jetzt mit `shared` gepaired." << std::endl;
+	        doesWeakExistProperly(weak);
+	        cout << "Noch in scope" << endl;
+	        cout << "Versuche nochmal doeskWeakExistProperly" << endl;
+	        doesWeakExistProperly(weak);
+	    }
+	    std::cout << "Der Pointer `shared` ist jetzt out of scope." << std::endl;
+	    doesWeakExistProperly(weak);
 	return 0;
 
 }
