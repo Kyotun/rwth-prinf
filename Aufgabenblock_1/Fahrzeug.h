5d7a3a8b811acf1e33be9b1bf751363b519eedcb
Kyotun <emirpisirici@hotmail.com>
2023-11-10 21:46:03 +0100

    Working on part 1 of the project

--- Aufgabenblock_1/Fahrzeug.h
+++ Aufgabenblock_1/Fahrzeug.h
@@ -7,11 +7,20 @@
 
 #ifndef FAHRZEUG_H_
 #define FAHRZEUG_H_
+#include <string>
+using namespace std;
 
 class Fahrzeug {
 public:
 	Fahrzeug();
+	Fahrzeug(string p_sName);
+	Fahrzeug(string p_sName, int p_iID);
 	virtual ~Fahrzeug();
+	string getName(){return p_sName;};
+private:
+	string p_sName = "";
+	const int p_iID = 0;
+	int p_iMaxID;
 };
 #endif /* FAHRZEUG_H_ */
 
--- Aufgabenblock_1/ab1.cpp
+++ Aufgabenblock_1/ab1.cpp
@@ -9,16 +9,63 @@
 #include <iostream>
 using namespace std;
 
-Fahrzeug::Fahrzeug() {
-	// TODO Auto-generated constructor stub
+Fahrzeug::Fahrzeug() { // @suppress("Class members should be properly initialized")
+
+	cout << "Standard Konstruktor Fahrzeug" << endl;
 
 }
 
 Fahrzeug::~Fahrzeug() {
-	// TODO Auto-generated destructor stub
+	cout << "Standard Destruktor Fahrzeug" << endl;
 }
 
+Fahrzeug::Fahrzeug(string p_sName){ // @suppress("Class members should be properly initialized")
+	this->p_sName = p_sName;
+	cout << "Ein Fahrzeug wurde mit dem Name " << p_sName <<  " erzeugt." << endl;
+}
+
+Fahrzeug::Fahrzeug(string p_sName, int p_iID): p_sName(p_sName), p_iID(p_iID) // @suppress("Class members should be properly initialized")
+{
+}
+
+void vAufgabe_1(){
+	int* dynamicElement = new int;
+	static int staticElement = 1;
+	int* dynamicElement2 = new int;
+	static int staticElement2 = 2;
+	delete dynamicElement;
+	delete dynamicElement2;
+}
 int main(){
-	cout << "A" << endl;
+	vAufgabe_1();
+	vector<unique_ptr<Fahrzeug>> unique_vector;
+	unique_ptr<Fahrzeug> unique_1 = make_unique<Fahrzeug>("unique_1");
+	unique_ptr<Fahrzeug> unique_2 = make_unique<Fahrzeug>("unique_2");
+
+	shared_ptr<Fahrzeug> shared_1 = make_shared<Fahrzeug>("shared_1");
+	shared_ptr<Fahrzeug> shared_2 = make_shared<Fahrzeug>("shared_2");
+	shared_ptr<Fahrzeug> shared_speichern = shared_2;
+
+	unique_vector.push_back(move(unique_1));
+	unique_vector.push_back(move(unique_2));
+
+
+	cout << "Count of shared_1: " << shared_1.use_count() << endl;
+	cout << "Count of shared_2: " << shared_2.use_count() << endl;
+	cout << "Count of shared_speichern: " << shared_speichern.use_count() << endl;
+
+
+	Fahrzeug a("emir", 5);
+	cout << a.getName() << endl;
+	if(unique_vector.front()){
+		cout << "in" << endl;
+		unique_vector.clear();
+		if(unique_vector.front()){
+			for(auto& i : unique_vector){
+					cout << i->getName() << endl;
+				}
+		}
+	}
+
 	return 0;
 }
